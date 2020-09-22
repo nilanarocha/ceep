e12337736cc805cf301a5463a3b996b6d79de8a0
Nilana Rocha <nilanaarquitetura@gmail.com>
2020-09-22 15:51:25 +1000

    creating a funcionality to element child list

--- todoList.js
+++ todoList.js
@@ -1,14 +1,17 @@
 const creatingANewTask = (event) => {
   event.preventDefault();
+
+  const list = document.querySelector('[data-list]');
   const input = document.querySelector('[data-form-input]');
-
   const value = input.value;
-  input.value = ' ';
 
-  const task = document.querySelector('[data-task]');
+  const task = document.createElement('li');
+  task.classList.add('task');
   const content = `<p class="content">${value}</p>`;
 
   task.innerHTML = content;
+  list.appendChild(task);
+  input.value = ' ';
 };
 
 const newTask = document.querySelector('[data-form-button]');
