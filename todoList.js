3727d423e2cba1f1585ebe2acc0dc833745b7738
Nilana Rocha <nilanaarquitetura@gmail.com>
2020-09-28 13:46:30 +1000

    adding the code into the one local scope

--- todoList.js
+++ todoList.js
@@ -1,19 +1,38 @@
-const creatingANewTask = (event) => {
-  event.preventDefault();
+(() => {
+  const creatingANewTask = (event) => {
+    event.preventDefault();
 
-  const list = document.querySelector('[data-list]');
-  const input = document.querySelector('[data-form-input]');
-  const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value;
 
-  const task = document.createElement('li');
-  task.classList.add('task');
-  const content = `<p class="content">${value}</p>`;
+    const task = document.createElement('li');
+    task.classList.add('task');
+    const content = `<p class="content">${value}</p>`;
 
-  task.innerHTML = content;
-  list.appendChild(task);
-  input.value = ' ';
-};
+    task.innerHTML = content;
+    task.appendChild(TickButton());
+    list.appendChild(task);
+    input.value = ' ';
+  };
 
-const newTask = document.querySelector('[data-form-button]');
+  const newTask = document.querySelector('[data-form-button]');
 
-newTask.addEventListener('click', creatingANewTask);
+  newTask.addEventListener('click', creatingANewTask);
+
+  const TickButton = () => {
+    const tickButton = document.createElement('button');
+    tickButton.innerText = 'Finish';
+    tickButton.classList.add('check-button');
+    tickButton.addEventListener('click', finishTask);
+    return tickButton;
+  };
+
+  const finishTask = (event) => {
+    const tickButton = event.target;
+
+    const taskDone = tickButton.parentElement;
+
+    taskDone.classList.toggle('done');
+  };
+})();
